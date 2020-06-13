3d7fa00189675842aa2b7dfa9ae8cbd1d9bdb2de
Gilson Polito <gilson.polito@exactaworks.com.br>
2020-06-13 20:58:35 -0300

    Exemplo de integração do chatful com a API que irá responder com os dados

--- src/main/java/br/com/infotrecho/response/ButtonsResponse.java
+++ src/main/java/br/com/infotrecho/response/ButtonsResponse.java
@@ -1,6 +1,5 @@
 package br.com.infotrecho.response;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
