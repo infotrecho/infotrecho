949ca720087fdf9a90418e5874e244ad6e38772a
Victor Giudicissi <victor.giudicissi@exactaworks.com.br>
2020-06-13 15:59:38 -0300

    Ajustes no log

--- src/main/java/com/boaviagem/boaviagem/controller/MessageController.java
+++ src/main/java/com/boaviagem/boaviagem/controller/MessageController.java
@@ -4,6 +4,7 @@ import static com.github.messenger4j.Messenger.CHALLENGE_REQUEST_PARAM_NAME;
 import static com.github.messenger4j.Messenger.MODE_REQUEST_PARAM_NAME;
 import static com.github.messenger4j.Messenger.SIGNATURE_HEADER_NAME;
 import static com.github.messenger4j.Messenger.VERIFY_TOKEN_REQUEST_PARAM_NAME;
+import static java.util.Optional.of;
 
 import com.boaviagem.boaviagem.service.MessageService;
 import com.github.messenger4j.Messenger;
@@ -33,7 +34,7 @@ public class MessageController {
     public ResponseEntity<String> verifyWebHook(@RequestParam(MODE_REQUEST_PARAM_NAME) final String mode,
                                                 @RequestParam(VERIFY_TOKEN_REQUEST_PARAM_NAME) final String verifyToken,
                                                 @RequestParam(CHALLENGE_REQUEST_PARAM_NAME) final String challenge) {
-        log.debug("Received Webhook verification request - mode: {} | verifyToken: {} | challenge: {}", mode, verifyToken, challenge);
+        log.info("Received Webhook verification request - mode: {} | verifyToken: {} | challenge: {}", mode, verifyToken, challenge);
         try {
             this.messenger.verifyWebhook(mode, verifyToken);
             return ResponseEntity.ok(challenge);
@@ -48,9 +49,27 @@ public class MessageController {
                                                @RequestHeader(SIGNATURE_HEADER_NAME) final String signature) {
         log.debug("Received Messenger Platform callback - payload: {} | signature: {}", payload, signature);
         try {
-            this.messenger.onReceiveEvents(payload, Optional.of(signature), event -> {
+            this.messenger.onReceiveEvents(payload, of(signature), event -> {
                 if (event.isTextMessageEvent()) {
                     this.messageService.handleTextMessageEvent(event.asTextMessageEvent());
+                } else if (event.isAttachmentMessageEvent()) {
+                    this.messageService.handleAttachmentMessageEvent(event.asAttachmentMessageEvent());
+                } else if (event.isQuickReplyMessageEvent()) {
+                    this.messageService.handleQuickReplyMessageEvent(event.asQuickReplyMessageEvent());
+                } else if (event.isPostbackEvent()) {
+                    this.messageService.handlePostbackEvent(event.asPostbackEvent());
+                } else if (event.isAccountLinkingEvent()) {
+                    this.messageService.handleAccountLinkingEvent(event.asAccountLinkingEvent());
+                } else if (event.isOptInEvent()) {
+                    this.messageService.handleOptInEvent(event.asOptInEvent());
+                } else if (event.isMessageEchoEvent()) {
+                    this.messageService.handleMessageEchoEvent(event.asMessageEchoEvent());
+                } else if (event.isMessageDeliveredEvent()) {
+                    this.messageService.handleMessageDeliveredEvent(event.asMessageDeliveredEvent());
+                } else if (event.isMessageReadEvent()) {
+                    this.messageService.handleMessageReadEvent(event.asMessageReadEvent());
+                } else {
+                    this.messageService.handleFallbackEvent(event);
                 }
             });
             log.debug("Processed callback payload successfully");
